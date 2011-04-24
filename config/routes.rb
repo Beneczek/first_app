656636c962e548d2e60a3b1af9cf76a3c4b7f37b
Jacek Pintera <easykey@interia.pl>
2011-04-24 12:40:29 +0200

    all

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ FirstApp::Application.routes.draw do
 
   resources :users
 
-  root_path :to => 'pages#home' 
+  root :to => 'pages#home' 
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
