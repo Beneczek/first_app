e5f1ebd4872ca1b6948f8f8fcaa0f13f3a86f400
Jacek Pintera <easykey@interia.pl>
2011-04-24 12:45:53 +0200

    all

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ FirstApp::Application.routes.draw do
 
   resources :users
 
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
   root :to => 'pages#home' 
 
   # The priority is based upon order of creation:
